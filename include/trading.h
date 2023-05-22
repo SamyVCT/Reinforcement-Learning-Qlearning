ecaa7f1410dda5f1ae3703ece5af172be2efc65b
Samy <samy.vincent10@gmail.com>
2023-05-22 17:22:55 +0200

    commentaires

--- include/trading.h
+++ include/trading.h
@@ -23,9 +23,16 @@ struct tradeOutput{
 
 typedef struct tradeOutput tradeOutput;
 
-
+/* INPUT : a = action à réaliser, prix_acquisition = combien vaut le titre possédé, prix_acquisition_old = combien vaut le précédent titre possédé, nb_titres = combien on possède de titres, stock_price = combien coute le titre sur le marché
+* OUTPUT : struct qui contient le reward accordé, done = si l'action est optimale, et les nouveaux prix d'acquisition et nombre de titres
+ * DESCRIPTION : Fonction qui effectue une étape de trading avec une action donnée
+ */
 tradeOutput trading_step(trade_action a, int prix_acquisition, int prix_acquisition_old, int nb_titres, int stock_price);
 
+/* INPUT : matrice des récompenses
+ * OUTPUT : void
+ * DESCRIPTION : Fonction qui affiche les étapes de trading par jour
+ */
 void render_trading(float** Q);
 
 #endif
